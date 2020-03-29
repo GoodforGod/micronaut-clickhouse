2d93e2c88f9148504a2777bcc8408cae5593fe8d
Anton Kurako <goodforgod.dev@gmail.com>
2020-03-29 10:24:13 +0300

    Database initializer test added for skip db

--- src/test/java/io/micronaut/configuration/clickhouse/ClickHouseDatabaseInitializationTests.java
+++ src/test/java/io/micronaut/configuration/clickhouse/ClickHouseDatabaseInitializationTests.java
@@ -44,4 +44,28 @@ class ClickHouseDatabaseInitializationTests extends Assertions {
 
         assertTrue(connection.createStatement().execute("SELECT * FROM custom.example"));
     }
+
+    @Test
+    void databaseDefaultInitializeSkip() throws Exception {
+        final Map<String, Object> properties = new HashMap<>();
+        properties.put("clickhouse.port", container.getFirstMappedPort());
+        properties.put("clickhouse.database", ClickHouseSettings.DEFAULT_DATABASE);
+        properties.put("clickhouse.createDatabaseIfNotExist", true);
+
+        final ApplicationContext context = ApplicationContext.run(properties);
+        final ClickHouseConnection connection = context.getBean(ClickHouseConnection.class);
+
+        final String version = connection.getServerVersion();
+        assertEquals("18.10.3", version);
+
+        assertTrue(connection.createStatement().execute(container.getTestQueryString()));
+
+        connection.createStatement().execute("CREATE TABLE default.example(" +
+                " name String," +
+                " registered DateTime " +
+                ") ENGINE = MergeTree() " +
+                " ORDER BY registered;");
+
+        assertTrue(connection.createStatement().execute("SELECT * FROM default.example"));
+    }
 }
